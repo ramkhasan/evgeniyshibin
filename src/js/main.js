eed26ce228d32831b9c880d98cd94e7d0a052184
Ramazan <ramhas87@gmail.com>
2021-12-22 15:15:36 +0300

    Верстка страницы со всеми проектами, Обо мне - главный блок и блок с навыками

--- src/js/main.js
+++ src/js/main.js
@@ -10,3 +10,18 @@ $(document).mouseup(function (e){
 $("a.item").click(function (){
   $("nav.main-nav, a.target-burger").removeClass("toggled");
 });
+
+// FAQ / Раскрывающиеся вкладки
+jQuery("document").ready(function ($) {
+  $(".dropdown__top").click(function () {
+    if ($(this).parent(".dropdown").hasClass("open")) {
+      $(this).parent(".dropdown").removeClass("open");
+      $(this).siblings(".dropdown__btm").slideUp(500);
+    } else {
+      $(".dropdown").removeClass("open");
+      $(".dropdown .dropdown__btm").slideUp(500);
+      $(this).parent(".dropdown").addClass("open");
+      $(this).siblings(".dropdown__btm").slideDown(500);
+    }
+  });
+});
