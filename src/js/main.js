f3bff4d6ea174410168d391979ad39271c0efd7f
Ramazan <ramhas87@gmail.com>
2021-12-23 23:00:43 +0300

    Верстка страницы Обо мне - блок с faq

--- src/js/main.js
+++ src/js/main.js
@@ -12,16 +12,14 @@ $("a.item").click(function (){
 });
 
 // FAQ / Раскрывающиеся вкладки
-jQuery("document").ready(function ($) {
-  $(".dropdown__top").click(function () {
-    if ($(this).parent(".dropdown").hasClass("open")) {
-      $(this).parent(".dropdown").removeClass("open");
-      $(this).siblings(".dropdown__btm").slideUp(500);
-    } else {
-      $(".dropdown").removeClass("open");
-      $(".dropdown .dropdown__btm").slideUp(500);
-      $(this).parent(".dropdown").addClass("open");
-      $(this).siblings(".dropdown__btm").slideDown(500);
-    }
-  });
+$(".dropdown__top").click(function () {
+  if ($(this).parent(".dropdown").hasClass("open")) {
+    $(this).parent(".dropdown").removeClass("open");
+    $(this).siblings(".dropdown__btm").slideUp(500);
+  } else {
+    $(".dropdown").removeClass("open");
+    $(".dropdown .dropdown__btm").slideUp(500);
+    $(this).parent(".dropdown").addClass("open");
+    $(this).siblings(".dropdown__btm").slideDown(500);
+  }
 });
